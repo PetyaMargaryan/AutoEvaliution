680808077d2cd688fec8292169758947d5dd774b
Petya <Petya Margaryan>
2023-05-28 21:59:48 +0400

    no finish

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -22,10 +22,6 @@
                                     <p class="text-gray-800 text-base px-6 mb-5">
                                         {{ $car->small_description }}
                                     </p>
-
-                                    <p class="text-gray-800 text-base px-6 mb-5">
-                                        {{ $car->small_description }}
-                                    </p>
                                 </div>
                             </div>
                         </a>
