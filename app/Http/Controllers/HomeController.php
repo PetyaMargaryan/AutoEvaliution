c946fd1896b4550fd7acb2504d770e2b4a4be2bc
Petya <Petya Margaryan>
2023-06-29 10:15:36 +0400

    Changed cips design

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,5 +70,9 @@ class HomeController extends Controller
     {
         dd($request->input());
     }
+    public function store2(Request $request)
+    {
+        dd($request->input());
+    }
 
 }
