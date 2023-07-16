164271b66564a7e44a76b0402fd3b641b5d661a3
Petya <Petya Margaryan>
2023-07-16 20:16:00 +0400

    Changed issue

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,6 +21,10 @@
                                 </div>
                                 <p class="text-gray-800 text-base px-6 mb-5">
                                     {{ $car->small_description }}
+                                    {{ $car->small_description }}
+                                    {{ $car->small_description }}
+                                    {{ $car->small_description }}
+                                    {{ $car->small_description }}
                                 </p>
                             </div>
                         </div>
