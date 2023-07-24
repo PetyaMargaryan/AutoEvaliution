bece7949d26ed5944778eed6caca09810c78d8ae
Petya <Petya Margaryan>
2023-07-24 12:56:24 +0400

    Created new page

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -177,6 +177,18 @@
                 <input class="image-file" id="main-image" type="file" name="main-image">
                 <input class="image-file" id="small-image-1" type="file" name="small-image-1">
                 <input class="image-file" id="small-image-2" type="file" name="small-image-2">
+            </div><div class="invisible">
+                <input class="image-file" id="main-image" type="file" name="main-image">
+                <input class="image-file" id="small-image-1" type="file" name="small-image-1">
+                <input class="image-file" id="small-image-2" type="file" name="small-image-2">
+            </div><div class="invisible">
+                <input class="image-file" id="main-image" type="file" name="main-image">
+                <input class="image-file" id="small-image-1" type="file" name="small-image-1">
+                <input class="image-file" id="small-image-2" type="file" name="small-image-2">
+            </div><div class="invisible">
+                <input class="image-file" id="main-image" type="file" name="main-image">
+                <input class="image-file" id="small-image-1" type="file" name="small-image-1">
+                <input class="image-file" id="small-image-2" type="file" name="small-image-2">
             </div>
 
             <button type="submit" class="text-white w-full justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
