6ac7618091ae8a89919d4dfbd993322f9e141a3f
Petya <Petya Margaryan>
2023-08-07 09:16:43 +0400

    Changed sub title

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -49,6 +49,14 @@
                                     </div>
                                     <p class="text-gray-800 text-base px-6 mb-5">
                                         {{ $car->small_description }}
+                                    </p><p class="text-gray-800 text-base px-6 mb-5">
+                                        {{ $car->small_description }}
+                                    </p><p class="text-gray-800 text-base px-6 mb-5">
+                                        {{ $car->small_description }}
+                                    </p><p class="text-gray-800 text-base px-6 mb-5">
+                                        {{ $car->small_description }}
+                                    </p><p class="text-gray-800 text-base px-6 mb-5">
+                                        {{ $car->small_description }}
                                     </p>
                                 </div>
                             </div>
