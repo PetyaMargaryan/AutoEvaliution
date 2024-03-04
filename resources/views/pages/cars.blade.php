c23a96b54836cefef358af1a3215253b7fbee284
Petya <Petya Margaryan>
2024-03-05 00:19:22 +0400

    Changed

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -14,7 +14,6 @@
                             </div>
                             <p class="text-gray-800 text-base px-6 mb-5">
                                 {{ $car->small_description }}
-
                             </p>
                         </div>
                     </div>
