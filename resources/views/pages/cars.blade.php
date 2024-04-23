9606869a165e82eded6bf0d84b2204c77de1f9b4
Petya <Petya Margaryan>
2024-04-23 21:03:33 +0400

    Changed

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -23,22 +23,8 @@
                 </div>
                 <div class="flex h-full overflow-hidden shadow-lg text-lg px-6 pb-2">
                     <p>{{ $car->description }}</p>
-                    <p>{{ $car->description }}</p>
-                    <p>{{ $car->description }}</p>
-                </div>
-                <div class="flex flex-wrap justify-between pt-5 -mx-6">
-                    @for($i = 0; $i <= 1; $i++)
-                        <div class="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
-                            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                                <img src="{{ $car->small_images[$i] }}" class="h-full w-full rounded-t">
-                                <img src="{{ $car->small_images[$i] }}" class="h-full w-full rounded-t">
-                                <img src="{{ $car->small_images[$i] }}" class="h-full w-full rounded-t">
-                                <img src="{{ $car->small_images[$i] }}" class="h-full w-full rounded-t">
-                            </div>
-                        </div>
-                    @endfor
-                </div>
 
+                </div>
                 <div class="flex h-full overflow-hidden shadow-lg pt-12 pb-5 px-6 font-bold">
                     <h3>Similar Vehicles</h3>
                 </div>
