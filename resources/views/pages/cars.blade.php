2e59dd32a4d5592de4410c37a67468e15a8e9712
Petya <Petya Margaryan>
2024-05-28 11:38:30 +0400

    Changed

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -58,14 +58,12 @@
                     <p>{{ $car->description }}</p>
                     <p>{{ $car->description }}</p>
                     <p>{{ $car->description }}</p>
-                    <p>{{ $car->description }}</p>
-                    <p>{{ $car->description }}</p>
                 </div>
                 <div class="flex flex-wrap justify-between pt-5 -mx-6">
                         @for($i = 0; $i <= 1; $i++)
                         <div class="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
                             <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                                <img src="{{ $car->small_images[$i] }}" class="h-full w-full rounded-t">
+                                <img src="{{ $car->small_images[$i] }}" class="h-full w-full rounded-t" alt="">
                             </div>
                         </div>
                     @endfor
